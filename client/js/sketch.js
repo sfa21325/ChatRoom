5ec8fd7c43b7423173ea42114ec5444815e09722
sfa21325 <155599396+sfa21325@users.noreply.github.com>
2024-05-28 04:32:46 +0000

    Setup basic game

--- client/js/sketch.js
+++ client/js/sketch.js
@@ -1,12 +1,27 @@
+var playernum;
+var playername;
 function preload() {
 
 }
 
 function setup() {
+    playernum = new Player("UntitledLoser2010");
     createCanvas(windowWidth, windowHeight);
-    background(200);
+    playername = document.getElementById("playername");
 }
 
 function draw() {
-    
+    background(200);
+    playernum.draw();
+    createP(playername);
+}
+
+var Player = function(name) {
+    this.name = name
+
+    this.draw = function() {
+        square(100, 100, 100);
+    }
+
+    return this;
 }
